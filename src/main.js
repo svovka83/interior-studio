48e1bd45462809541c794330912fd1ca8b4d7cc1
Volodymyr Smoliak <svovka83@gmail.com>
2026-03-24 10:23:01 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,122 @@
+import "./scss/styles.scss";
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
+
+// drop-down
+
+const selects = document.querySelectorAll(".select");
+
+selects.forEach((select) => {
+	const control = select.querySelector(".select__control");
+	const dropdown = select.querySelector(".select__dropdown");
+	const chevron = select.querySelector(".select__chevron");
+	const value = select.querySelector(".select__value");
+	const options = select.querySelectorAll(".select__option");
+
+	control.addEventListener("click", () => {
+		dropdown.classList.toggle("select__dropdown_open");
+		chevron.classList.toggle("select__chevron_open");
+	});
+
+	options.forEach((option) => {
+		option.addEventListener("click", () => {
+			value.textContent = option.textContent;
+
+			dropdown.classList.remove("select__dropdown_open");
+			chevron.classList.remove("select__chevron_open");
+		});
+	});
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach(
+			(entry) => {
+				if (!entry.isIntersecting) {
+					entry.target.classList.toggle("select__dropdown_show-above");
+				}
+			},
+			{
+				threshold: 0.5,
+			}
+		);
+	});
+	observer.observe(dropdown);
+});
+
+// main-swiper
+
+const mainTextSlider = new Swiper(".main-top__swiper-text", {
+	slidesPerView: 1,
+	speed: 1200,
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
+	allowTouchMove: false,
+});
+
+const mainImageSlider = new Swiper(".main-top__swiper-img", {
+	initialSlide: 0,
+	speed: 800,
+	allowTouchMove: false,
+
+	thumbs: {
+		swiper: mainTextSlider,
+	},
+
+	pagination: {
+		el: ".main-top-swiper-pagination",
+		type: "progressbar",
+	},
+
+	navigation: {
+		nextEl: ".main-top-swiper-next",
+		prevEl: ".main-top-swiper-prev",
+	},
+	breakpoints: {
+		321: {
+			slidesPerView: 1.195,
+		},
+		378: {
+			slidesPerView: 1.17,
+		},
+		769: {
+			slidesPerView: 1.185,
+		},
+		1025: {
+			slidesPerView: 1.175,
+		},
+	},
+});
+
+const prevBtn = document.getElementById("progress-prev");
+const nextBtn = document.getElementById("progress-next");
+const nextText = document.getElementById("progress-num-next");
+let counterProgress = 1;
+
+// interrupts running code
+prevBtn.addEventListener("click", () => {
+	if (counterProgress > 1) {
+		counterProgress--;
+		nextText.innerHTML = `${counterProgress}`;
+	}
+});
+nextBtn.addEventListener("click", () => {
+	if (counterProgress < 4) {
+		counterProgress++;
+		nextText.innerHTML = `${counterProgress}`;
+	}
+});
+
+// toolbar-swiper
+
+new Swiper(".toolbar__swiper", {
+	initialSlide: 4,
+	slidesPerView: 3,
+	spaceBetween: 54,
+	speed: 500,
+	allowTouchMove: false,
+
+	navigation: {
+		nextEl: ".toolbar-swiper-next",
+		prevEl: ".toolbar-swiper-prev",
+	},
+});
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
+import nunjucks from "vite-plugin-nunjucks";
+import { resolve } from "path";
+
+export default defineConfig({
+	plugins: [Inspect(), nunjucks()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				news: resolve(__dirname, "src/pages/news/index.html"),
+				oneNews: resolve(__dirname, "src/pages/one-news/index.html"),
+			},
+		},
+	},
+	// resolve: {
+	// 	alias: {
+	// 		"@img": resolve(__dirname, "public/images"),
+	// 	},
+	// },
+});
