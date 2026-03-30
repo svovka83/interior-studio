06776a1a3880934d4176e3c7b6b4221ab69a54dd
Volodymyr Smoliak <svovka83@gmail.com>
2026-03-31 02:30:07 +0300

    popups

--- src/main.js
+++ src/main.js
@@ -39,8 +39,8 @@ burgerBtn.forEach((btn) => {
 // pop-up form
 
 const openPopup = document.querySelectorAll(".button");
-const closePopup = document.querySelector(".pop-up__close");
 const popup = document.querySelector(".pop-up");
+const closePopup = document.querySelector(".pop-up__close");
 
 openPopup.forEach((open) => {
 	open.addEventListener("click", () => {
@@ -51,6 +51,30 @@ openPopup.forEach((open) => {
 
 closePopup.addEventListener("click", () => {
 	popup.classList.remove("pop-up_open");
+	enableBodyScroll(document.body);
+});
+
+// pop-up 2
+
+const openPopup2 = document.querySelector(".pop-up__button");
+const popup2 = document.querySelector(".pop-up-2");
+
+const closePopup2 = document.querySelector(".pop-up__close-2");
+const submit = document.querySelector(".pop-up-2__button");
+
+openPopup2.addEventListener("click", () => {
+	popup.classList.remove("pop-up_open");
+	popup2.classList.add("pop-up-2_open");
+	disableBodyScroll(document.body);
+});
+
+closePopup2.addEventListener("click", () => {
+	popup2.classList.remove("pop-up-2_open");
+	enableBodyScroll(document.body);
+});
+submit.addEventListener("click", () => {
+	popup.classList.remove("pop-up_open");
+	popup2.classList.remove("pop-up-2_open");
 	enableBodyScroll(document.body);
 });
 
