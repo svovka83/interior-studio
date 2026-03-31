873a2f3de74c0f0d7d86cb1087aa7cd671ccadc0
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-01 01:14:24 +0300

    added resize to burger-menu

--- src/js/utils/burger-menu.js
+++ src/js/utils/burger-menu.js
@@ -5,6 +5,24 @@ export function initBurgerMenu() {
 	const burgerMenu = document.querySelectorAll(".burger-menu-screen");
 	const burgerBottom = document.querySelectorAll(".main-top__bottom.burger-menu-bottom");
 
+	window.addEventListener("resize", () => {
+		burgerMenu.forEach((menu) => {
+			if (menu.classList.contains("burger-menu-screen_active")) {
+				setTimeout(() => {
+					menu.classList.remove("burger-menu-screen_active");
+					enableBodyScroll(document.body);
+				}, 950);
+			}
+		});
+	});
+	window.addEventListener("resize", () => {
+		burgerBottom.forEach((bottom) => {
+			if (bottom.classList.contains("burger-menu-bottom_active")) {
+				bottom.classList.remove("burger-menu-bottom_active");
+			}
+		});
+	});
+
 	burgerBtn.forEach((btn) => {
 		btn.addEventListener("click", () => {
 			burgerMenu.forEach((menu) => {
