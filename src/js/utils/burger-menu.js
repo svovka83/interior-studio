0ccbc7994fbfeb7b63da842911bdef3d564377a1
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-01 03:48:43 +0300

    animation burger icon

--- src/js/utils/burger-menu.js
+++ src/js/utils/burger-menu.js
@@ -4,6 +4,8 @@ export function initBurgerMenu() {
 	const burgerBtn = document.querySelectorAll(".burger-menu");
 	const burgerMenu = document.querySelectorAll(".burger-menu-screen");
 	const burgerBottom = document.querySelectorAll(".main-top__bottom.burger-menu-bottom");
+	const openIcon = document.querySelectorAll(".burger-menu__icon.burger-menu__icon-open");
+	const closeIcon = document.querySelectorAll(".burger-menu__icon.burger-menu__icon-close");
 
 	window.addEventListener("resize", () => {
 		burgerMenu.forEach((menu) => {
@@ -14,11 +16,21 @@ export function initBurgerMenu() {
 				}, 950);
 			}
 		});
-	});
-	window.addEventListener("resize", () => {
 		burgerBottom.forEach((bottom) => {
 			if (bottom.classList.contains("burger-menu-bottom_active")) {
 				bottom.classList.remove("burger-menu-bottom_active");
+			}
+		});
+		openIcon.forEach((icon) => {
+			if (icon.classList.contains("burger-menu__icon-open_active")) {
+				setTimeout(() => {
+					icon.classList.remove("burger-menu__icon-open_active");
+				}, 700);
+			}
+		});
+		closeIcon.forEach((icon) => {
+			if (icon.classList.contains("burger-menu__icon-close_active")) {
+				icon.classList.remove("burger-menu__icon-close_active");
 			}
 		});
 	});
@@ -41,6 +53,24 @@ export function initBurgerMenu() {
 					bottom.classList.add("burger-menu-bottom_active");
 				} else {
 					bottom.classList.remove("burger-menu-bottom_active");
+				}
+			});
+			openIcon.forEach((icon) => {
+				if (!icon.classList.contains("burger-menu__icon-open_active")) {
+					icon.classList.add("burger-menu__icon-open_active");
+				} else {
+					setTimeout(() => {
+						icon.classList.remove("burger-menu__icon-open_active");
+					}, 700);
+				}
+			});
+			closeIcon.forEach((icon) => {
+				if (!icon.classList.contains("burger-menu__icon-close_active")) {
+					setTimeout(() => {
+						icon.classList.add("burger-menu__icon-close_active");
+					}, 700);
+				} else {
+					icon.classList.remove("burger-menu__icon-close_active");
 				}
 			});
 		});
