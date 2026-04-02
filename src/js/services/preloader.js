cd17003e59f8817da8f4334e987931af74108fdc
Volodymyr Smoliak <svovka83@gmail.com>
2026-04-03 02:00:08 +0300

    preloader with count percents

--- src/js/services/preloader.js
+++ src/js/services/preloader.js
@@ -1,13 +1,35 @@
 export function preloader() {
-	window.addEventListener("load", () => {
-		const loader = document.getElementById("preloader");
-		setTimeout(() => {
-			loader.style.opacity = "0";
-			loader.style.transition = "opacity 1s";
-		}, 1500);
+	const images = Array.from(document.images);
+	let loaded = 0;
+	const loader = document.getElementById("preloader");
+	const loaderPercets = document.querySelector(".preloader__percents");
 
-		setTimeout(() => {
-			loader.remove();
-		}, 2800);
+	const check = () => {
+		loaded++;
+
+		const percent = Math.round((loaded / images.length) * 100);
+		loaderPercets.textContent = percent + "%";
+
+		if (loaded === images.length) {
+			setTimeout(() => {
+				loader.style.opacity = "0";
+				loader.style.transition = "opacity 1s";
+			}, 1500);
+
+			setTimeout(() => {
+				loader.remove();
+			}, 2800);
+		}
+	};
+
+	if (images.length === 0) check();
+
+	images.forEach((img) => {
+		if (img.complete) {
+			check();
+		} else {
+			img.addEventListener("load", check);
+			img.addEventListener("error", check);
+		}
 	});
 }
